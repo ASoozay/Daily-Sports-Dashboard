a0302c020a427c12c11fb0a7c94f46b6170c4396
ASoozay <asousa2021@gmail.com>
2026-02-28 14:48:37 -0800

    v1 of available

---
+++ netlify/functions/available-games.js
@@ -0,0 +1,32 @@
+const { Client } = require("pg");
+
+exports.handler = async (event) => {
+  try {
+    const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        require: true,
+        rejectUnauthorized: false
+    }
+    });
+
+    await client.connect();
+
+    const availableGames = await client.query(
+      'SELECT * FROM "Games" WHERE available = TRUE'
+    );
+
+    await client.end();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ games: availableGames.rows })
+    };
+
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: err.message })
+    };
+  }
+};
