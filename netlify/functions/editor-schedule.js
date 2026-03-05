061e17a82158bc6d5faeb15a95ac7c31582e48ee
ASoozay <asousa2021@gmail.com>
2026-03-05 01:06:02 -0800

    add editor page

---
+++ netlify/functions/editor-schedule.js
@@ -0,0 +1,64 @@
+const { Client } = require("pg");
+
+exports.handler = async (event) => {
+  try {
+    const body = JSON.parse(event.body);
+    const writerId = body.writerId;
+    const { filters = {} } = body;
+    const { sports = [], locations = [] } = filters;
+
+    console.log("Request body:", body);
+
+    if (!writerId) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ error: "writerId missing" }),
+      };
+    }
+
+    const client = new Client({
+      connectionString: process.env.DATABASE_URL,
+      ssl: { require: true, rejectUnauthorized: false },
+    });
+
+    await client.connect();
+
+    let query = `SELECT * FROM "Assignments" 
+                JOIN "Games" ON "Games".game_id = "Assignments".game_id
+                JOIN "Writers" ON "Writers".writer_id = "Assignments".writer_id 
+                WHERE date >= CURRENT_DATE`;
+
+    if (sports.length > 0) {
+      values.push(sports);
+      query += ` AND sport = ANY($${values.length})`;
+    }
+
+    if (locations.length === 1) {
+      if (locations[0] === "Home") {
+        query += ` AND location = 'Seattle, Wash.'`;
+      } else if (locations[0] === "Away") {
+        query += ` AND location != 'Seattle, Wash.'`;
+      }
+    }
+
+    query += ` ORDER BY date, time`;
+
+    console.log("Final query:", query);
+    console.log("Values:", values);
+
+    const result = await client.query(query, values);
+
+    await client.end();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ games: result.rows }),
+    };
+  } catch (err) {
+    console.error("Error fetching scheduled games:", err);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: err.message }),
+    };
+  }
+};
