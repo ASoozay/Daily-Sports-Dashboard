91d8f282cd17122c4de9f1461dc69e3fd4f7c4df
ASoozay <asousa2021@gmail.com>
2026-03-05 01:06:02 -0800

    add editor page

--- pages/js/editordash.js
+++ pages/js/editordash.js
@@ -34,12 +34,6 @@
                 document.getElementById("greetingHeader").textContent = "Hi, Guest";
             }
 
-            if (currWriter.position === "Editor") {
-                window.location.href = "/pages/editor.html";
-            } else {
-                window.location.href = "/pages/dashboard/dashboard.html";
-            }
-
             return currWriter; // <- return so we can await
         };
 
@@ -97,7 +91,7 @@
                 return;  // Exit if writerId is missing
             }
             
-            const response = await fetch("/.netlify/functions/scheduled-games", {
+            const response = await fetch("/.netlify/functions/editor-schedule", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ writerId, filters }) 
@@ -133,6 +127,7 @@
                 });
                 const time = game.time;
                 const notes = game.notes;
+                const name = game.first_name + " " + game.last_name;
                 let where = "";
                 let recap = "";
                 let recap_css = "";
@@ -161,9 +156,15 @@
                     <div class="${recap_css}"></div>
                     <p class="recap-location">${location}</p>
                 </div>
-                <div class = "date">${date}</div>
-                <div class = "time">${time}</div>
-                <button class = "remove" data-game-id = "${gameId}"></div>
+                <div class = "when-container>
+                    <div class = "date">${date}</div>
+                    <div class = "time">${time}</div>
+                </div>
+                <div class = "writer">${name}</div>
+                <div class = "option-container"> 
+                    <button class = "submit"></div>  
+                    <button class = "remove" data-game-id = "${gameId}"></div>
+                </div>    
             `;
 
             container.appendChild(gameBox);
