5ee7eb2fd1e3882e503a6c1dd3557b42cde1d19c
ASoozay <asousa2021@gmail.com>
2026-03-23 10:49:33 -0700

    test credential agaibn

--- netlify/functions/credential-reminder-background.js
+++ netlify/functions/credential-reminder-background.js
@@ -5,7 +5,7 @@ import sgMail from "@sendgrid/mail";
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 export const config = {
-  schedule: "* 7 * * *",
+  schedule: "0 11 * * *",
   timezone: "America/Los_Angeles"
 };
 
