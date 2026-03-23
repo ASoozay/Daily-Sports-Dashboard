6f8d94295308033b69125e039fa9e9ab65c33b7a
ASoozay <asousa2021@gmail.com>
2026-03-22 17:06:56 -0700

    test game reminder

--- netlify/functions/game-reminder-background.js
+++ netlify/functions/game-reminder-background.js
@@ -3,7 +3,7 @@ import sgMail from "@sendgrid/mail";
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 export const config = {
-  schedule: "0 7 * * *",
+  schedule: "* * * * *",
   timezone: "America/Los_Angeles"
 };
 
