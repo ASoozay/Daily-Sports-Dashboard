b85910e5eed5308b6bf1981ecd0ade5fccfd496b
ASoozay <asousa2021@gmail.com>
2026-04-08 20:21:34 -0700

    add get-invoics hs

---
+++ netlify/functions/get-invoices.js
@@ -0,0 +1,35 @@
+const { Client } = require("pg");
+
+exports.handler = async (event) => {
+  try {
+    const { writerId } = JSON.parse(event.body);
+
+    const client = new Client({
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    });
+
+    await client.connect();
+
+    // Base query
+    let query = `SELECT * FROM "Invoices" WHERE writer_id = $1 ORDER BY date DESC`;
+
+    const invoices = await client.query(query, [writerId]);
+
+    await client.end();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ invoice: invoices.rows }),
+    };
+  } catch (err) {
+    console.error("Error fetching invoices:", err);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: err.message }),
+    };
+  }
+};
