81b8fcd4ff2715d45135e46f26aa3c5c21df8c2f
ASoozay <asousa2021@gmail.com>
2026-05-02 19:46:24 -0700

    add school stuff

---
+++ js/schools.js
@@ -0,0 +1,17 @@
+let sports = [];
+let conferences = [];
+let schools = [];
+
+/* Fetch */
+async function fetchSchools() {
+    try {
+        const response = await fetch("/.netlify/functions/get-schools", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ sports, conferences, schools })
+        });
+
+    } catch (error) {
+        console.error('Error fetching schools:', error);
+    }
+}
